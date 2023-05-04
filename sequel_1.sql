40774d7a4e8c967d927d64da4726d817d81a02ca
Gulshad <azgulj@gmail.com>
2023-05-04 23:16:48 +0300

    1hour of sql course

--- sequel_1.sql
+++ sequel_1.sql
@@ -14,3 +14,57 @@ SELECT
     rental_id + 100 --here
 FROM payment;
 --end
+
+--task in course: 10% more expensive 
+SELECT 
+    first_name AS name,
+    price AS 'unit price',
+    price*1.1 AS 'new price'
+FROM customer;
+--end
+
+--using WHERE and logical operators AND/OR
+SELECT * 
+FROM Customers
+WHERE points > 1000 
+    OR order_date >= '1990-01-01' 
+    AND state = 'VA'
+--end
+
+--using NOT
+SELECT *
+FROM Customers
+WHERE NOT (points > 1000 OR state = 'VA')
+--end
+
+--task in course: order #6 and total price greater than 30
+SELECT *
+FROM order_items 
+WHERE order_id = 6
+    AND unit_price * quantity > 30
+--end
+
+-- using IN/NOT IN
+SELECT *
+FROM products
+WHERE quantity_in_stock IN (42, 38, 79) 
+                --NOT IN (42, 38, 79)
+--end
+
+-- using BETWEEN
+SELECT *
+FROM Customers
+WHERE points BETWEEN 1000 AND 3000
+--end
+
+--task in course
+SELECT *
+FROM Customers
+WHERE birth_date BETWEEN '1990-01-01' AND '2000-01-01'
+--end
+
+--using LIKE
+SELECT *
+FROM Customers
+WHERE first_name LIKE '_b%' -- '_' - means 1 symbol, '%' means any number of symbols
+--end
