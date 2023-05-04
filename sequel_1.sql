ddd788ec639089d00f5074a60631aca65438be25
Gulshad <azgulj@gmail.com>
2023-05-04 22:22:02 +0300

    select1

--- sequel_1.sql
+++ sequel_1.sql
@@ -4,5 +4,13 @@ SELECT *
 FROM actor 
 WHERE first_name = "a%"
 ORDER BY last_name;
+--end
 
+-- using calculations after SELECT
+USE sakila;
+SELECT 
+	payment_id,
+    customer_id,
+    rental_id + 100 --here
+FROM payment;
 --end
